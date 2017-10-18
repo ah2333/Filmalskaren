959a41545a593bccfa80799c8aa414da4de36cdb
elinrossander <elinnrossander@gmail.com>
2017-10-18 15:15:52 +0200

    Ändring

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request
+request.headers.get('Accept')
 app = Flask(__name__, static_url_path = "",static_folder= "")
 
-@app.route("/")
+@app.route('/')
 def index_html():
     return render_template('index_html')
 
