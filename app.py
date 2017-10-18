34e20d477697235ec3e5a3d30c6b3165bb02e7a8
elinrossander <elinnrossander@gmail.com>
2017-10-18 15:52:38 +0200

    Fått upp css med länkad static

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request
-request.headers.get('Accept')
-app = Flask(__name__, static_url_path = "",static_folder= "")
+app = Flask(__name__, static_url_path = "",static_folder = "")
 
 @app.route('/')
 def index_html():
-    return render_template('index_html')
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
