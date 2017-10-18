e307a5858f220bf59b5771c61bc0e30e37fff5a2
elinrossander <elinnrossander@gmail.com>
2017-10-18 15:01:39 +0200

    Uppdaterat kod

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+app = Flask(__name__, static_url_path = "",static_folder= "")
 
 @app.route("/")
 def index_html():
     return render_template('index_html')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
