eaa2e7d45009dd6bc84029f5226c0a500569f8c6
ah2333 <ellinor.maveus@hotmail.com>
2017-10-26 09:36:18 +0200

    Ny pythonkod

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request
 app = Flask(__name__, static_url_path = "",static_folder = "")
@@ -6,6 +5,26 @@ app = Flask(__name__, static_url_path = "",static_folder = "")
 @app.route('/')
 def index_html():
     return render_template('index.html')
+
+params= {
+"api_key": "AIzaSyBR8d23Es_KgpAqNFDDdPFRYZsk2Z5jKWo",
+"q": "",
+"type": "video",
+"videoDuration": "short",
+"videoEmbeddable": "true",
+"part": "snippet",
+"maxResults": "3"
+}
+
+r = requests.get('https://www.googleapis.com/youtube/v3/search', params=params)
+print(r.text)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
